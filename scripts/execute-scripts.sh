5b8b0a2b1b49904b44ca1a307a9806913e96f561
Alfa Yohannis <alfa.yohannis@gmail.com>
2021-10-14 15:38:20 +0100

    Test SparQL performance

---
+++ scripts/execute-scripts.sh
@@ -0,0 +1,6 @@
+# run code on self-signed localhost solid node server, all the three variables should be defined2
+#export NODE_TLS_REJECT_UNAUTHORIZED=0 SOLID_USERNAME=alfa SOLID_PASSWORD=Lololol3x! SOLID_IDP=https://localhost:8443 && node index-localhost-alfa.js
+
+export NODE_TLS_REJECT_UNAUTHORIZED=0 SOLID_USERNAME=alice SOLID_PASSWORD=Lololol3x! SOLID_IDP=https://localhost:9101 && node create-posts-localhost.js
+
+
